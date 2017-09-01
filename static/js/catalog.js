85c61f42be36c82d98d4e0ff4ae96634de12dc8c
evblance <evblance@users.noreply.github.com>
2017-09-01 18:57:17 +1000

    Adds comments to functions

--- static/js/catalog.js
+++ static/js/catalog.js
@@ -1,27 +1,36 @@
+/*
+ * CATALOG.js - Main scripts for Web UI
+ */
+
 
 (function(jQuery) {
 
   'use strict';
 
+  // Animates item admin panel options into view
   function showItemAdminPanel() {
     $(this).find('.admin-panel--item')
     .addClass('admin-panel--item--shown');
   }
 
+  // Animates item admin panel options out of view
   function hideItemAdminPanel() {
     $('.admin-panel--item').removeClass('admin-panel--item--shown');
   }
 
+  // Animates category admin panel options into view
   function showCategoryAdminPanel() {
     $('.admin-panel--category').removeClass('admin-panel--category--shown');
     $(this).find('.admin-panel--category')
     .toggleClass('admin-panel--category--shown');
   }
 
+  // Animates category admin panel options out of view
   function hideCategoryAdminPanel() {
     $('.admin-panel--category').removeClass('admin-panel--category--shown');
   }
 
+  // Toggles category images from view
   function toggleCategoryImage() {
     $(this).find('.list__item--categories__img')
     .toggleClass('list__item--categories__img--active');
