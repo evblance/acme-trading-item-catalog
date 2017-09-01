1264bf59c1f9f54106fbb780ad80a878ece96157
evblance <evblance@users.noreply.github.com>
2017-09-01 18:45:31 +1000

    Wraps JS code in anonymous function and applies strict mode

--- static/js/catalog.js
+++ static/js/catalog.js
@@ -1,36 +1,41 @@
 
+(function(jQuery) {
 
-function showItemAdminPanel() {
-  $(this).find('.admin-panel--item')
-         .addClass('admin-panel--item--shown');
-}
+  'use strict';
 
-function hideItemAdminPanel() {
-  $('.admin-panel--item').removeClass('admin-panel--item--shown');
-}
+  function showItemAdminPanel() {
+    $(this).find('.admin-panel--item')
+    .addClass('admin-panel--item--shown');
+  }
 
-function showCategoryAdminPanel() {
-  $('.admin-panel--category').removeClass('admin-panel--category--shown');
-  $(this).find('.admin-panel--category')
-         .toggleClass('admin-panel--category--shown');
-}
+  function hideItemAdminPanel() {
+    $('.admin-panel--item').removeClass('admin-panel--item--shown');
+  }
 
-function hideCategoryAdminPanel() {
-  $('.admin-panel--category').removeClass('admin-panel--category--shown');
-}
+  function showCategoryAdminPanel() {
+    $('.admin-panel--category').removeClass('admin-panel--category--shown');
+    $(this).find('.admin-panel--category')
+    .toggleClass('admin-panel--category--shown');
+  }
 
-function toggleCategoryImage() {
-  $(this).find('.list__item--categories__img')
-         .toggleClass('list__item--categories__img--active');
-}
+  function hideCategoryAdminPanel() {
+    $('.admin-panel--category').removeClass('admin-panel--category--shown');
+  }
 
-var $main = $('.main');
+  function toggleCategoryImage() {
+    $(this).find('.list__item--categories__img')
+    .toggleClass('list__item--categories__img--active');
+  }
 
-$main.on('mouseenter', '.list__item--items', showItemAdminPanel);
-$main.on('mouseleave', '.admin-panel--item', hideItemAdminPanel);
+  var $main = $('.main');
 
-$main.on('mouseenter', '.list__item--categories', showCategoryAdminPanel);
-$main.on('mouseleave', '.list__item--categories', hideCategoryAdminPanel);
+  $main.on('mouseenter', '.list__item--items', showItemAdminPanel);
+  $main.on('mouseleave', '.admin-panel--item', hideItemAdminPanel);
 
-$main.on('mouseenter', '.list__item--categories', toggleCategoryImage);
-$main.on('mouseleave', '.list__item--categories', toggleCategoryImage);
+  $main.on('mouseenter', '.list__item--categories', showCategoryAdminPanel);
+  $main.on('mouseleave', '.list__item--categories', hideCategoryAdminPanel);
+
+  $main.on('mouseenter', '.list__item--categories', toggleCategoryImage);
+  $main.on('mouseleave', '.list__item--categories', toggleCategoryImage);
+
+})($);
